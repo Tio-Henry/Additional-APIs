ea307279ea698a6b3d0730e6772ab9212539a211
MrL13 <H3nry.g147@gmail.com>
2023-03-11 16:41:01 -0300

    save

--- APIs/gamejolt_api.gd
+++ APIs/gamejolt_api.gd
@@ -1,1 +1,11 @@
 extends Node
+
+var web = "https://api.gamejolt.com/api/game/v1_2/"
+var _http_request = HTTPRequest.new()
+
+func _ready():
+	add_child(_http_request)
+	
+func add_score(game_id: String, user_token: String, username: String, privite_key, score: String):
+	_http_request.request(web + "score/add/" + "?game_id=" + game_id + "&username=" + username + "&user_token=" + user_token + "&score=" + score + "&signature=" + )
+	pass
--- Additional-APIs.gd
+++ Additional-APIs.gd
@@ -4,9 +4,11 @@ extends EditorPlugin
 
 func _enter_tree():
 	# Initialization of the plugin goes here.
+	add_autoload_singleton("GameJoltAPI", "res://addons/Additional-APIs/APIs/gamejolt_api.gd")
 	pass
 
 
 func _exit_tree():
 	# Clean-up of the plugin goes here.
+	remove_autoload_singleton("GameJoltAPI")
 	pass
