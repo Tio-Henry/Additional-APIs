8d6707b023221ee1cfb53bc6a8297ae91abff4a3
MrL13 <H3nry.g147@gmail.com>
2023-03-13 18:27:37 -0300

    new

--- Additional-APIs.gd
+++ Additional-APIs.gd
@@ -1,15 +1,17 @@
 @tool
 extends EditorPlugin
 
+var GJC_DOCK = preload("res://addons/Additional-APIs/UIs/gamejolt_ui.tscn").instantiate()
 
 func _enter_tree():
 	# Initialization of the plugin goes here.
 	add_autoload_singleton("GameJoltAPI", "res://addons/Additional-APIs/APIs/gamejolt_api.gd")
-	add_control_to_bottom_panel()
+	add_control_to_container(EditorPlugin.CONTAINER_CANVAS_EDITOR_BOTTOM, GJC_DOCK)
 	pass
 
 
 func _exit_tree():
 	# Clean-up of the plugin goes here.
+	remove_control_from_container(EditorPlugin.CONTAINER_CANVAS_EDITOR_BOTTOM, GJC_DOCK)
 	remove_autoload_singleton("GameJoltAPI")
 	pass
