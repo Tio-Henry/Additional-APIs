6e8c072b276efec2cd9f391707df8c1e767d8daf
MrL13 <H3nry.g147@gmail.com>
2023-03-13 17:04:27 -0300

    new

--- Additional-APIs.gd
+++ Additional-APIs.gd
@@ -5,6 +5,7 @@ extends EditorPlugin
 func _enter_tree():
 	# Initialization of the plugin goes here.
 	add_autoload_singleton("GameJoltAPI", "res://addons/Additional-APIs/APIs/gamejolt_api.gd")
+	add_control_to_bottom_panel()
 	pass
 
 
