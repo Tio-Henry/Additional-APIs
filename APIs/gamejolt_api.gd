4228b835cfd88702aec50bb58f19623c6a4b284c
Tio-Henry <H3nry.g147@gmail.com>
2023-03-18 16:53:47 -0300

    update

--- APIs/gamejolt_api.gd
+++ APIs/gamejolt_api.gd
@@ -7,5 +7,5 @@ func _ready():
 	add_child(_http_request)
 	
 func add_score(game_id: String, user_token: String, username: String, privite_key, score: String):
-	_http_request.request(web + "score/add/" + "?game_id=" + game_id + "&username=" + username + "&user_token=" + user_token + "&score=" + score + "&signature=" + )
+#	_http_request.request(web + "score/add/" + "?game_id=" + game_id + "&username=" + username + "&user_token=" + user_token + "&score=" + score + "&signature=" + )
 	pass
