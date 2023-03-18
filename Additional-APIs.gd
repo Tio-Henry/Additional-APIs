f8586fa9e624297cd9c00b18363ae5f18ccf135a
Tio-Henry <H3nry.g147@gmail.com>
2023-03-18 16:09:18 -0300

    update

--- Additional-APIs.gd
+++ Additional-APIs.gd
@@ -2,29 +2,13 @@
 extends EditorPlugin
 
 var plugin_dock = preload("res://addons/Additional-APIs/plugin_dock.tscn").instantiate()
-var gj_gid_info = {
-	"name":"Additional-APIs/Game_Jolt/game_id",
-	"type": TYPE_STRING
-}
-var gj_pi_info = {
-	"name":"Additional-APIs/Game_Jolt/game_id",
-	"type": TYPE_STRING
-}
 
 func _enter_tree():
-	# Initialization of the plugin goes here.
+	add_control_to_container(EditorPlugin.CONTAINER_PROJECT_SETTING_TAB_LEFT, plugin_dock)
 	add_autoload_singleton("GameJoltAPI", "res://addons/Additional-APIs/APIs/gamejolt_api.gd")
-	ProjectSettings.add_property_info(gj_gid_info)
-	ProjectSettings.add_property_info(gj_pi_info)
 	pass
-
-func _has_main_screen():
-	return true
 	
-func _make_visible(visible):
-	plugin_dock.visible = visible
-
 func _exit_tree():
-	# Clean-up of the plugin goes here.
+	remove_control_from_container(EditorPlugin.CONTAINER_PROJECT_SETTING_TAB_LEFT, plugin_dock)
 	remove_autoload_singleton("GameJoltAPI")
 	pass
