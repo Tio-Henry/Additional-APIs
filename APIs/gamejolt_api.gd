f41b1d0f7eb25f0080f147a3e4c99ffa108f7fde
Tio-Henry <H3nry.g147@gmail.com>
2023-03-22 16:04:14 -0300

    Game Jolt API

--- APIs/gamejolt_api.gd
+++ APIs/gamejolt_api.gd
@@ -2,13 +2,8 @@ extends Node
 
 var web = "https://api.gamejolt.com/api/game/v1_2/"
 var _http_request = HTTPRequest.new()
-var private_key = "b029daca5e31001ba305c5db528019da"
-var _sha1_key = private_key.sha1_text()
 
-func _ready():
-	add_child(_http_request)
-	print(_sha1_key)
+
+func connect_api(TYPE: String):
 	
-func add_score(game_id: String, user_token: String, username: String, private_key, score: String):
-#	_http_request.request(web + "score/add/" + "?game_id=" + game_id + "&username=" + username + "&user_token=" + user_token + "&score=" + score + "&signature=" + )
 	pass
