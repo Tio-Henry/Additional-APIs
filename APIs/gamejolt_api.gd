a645589fbc57136fa57177345ddc0e42eefba1f0
Tio-Henry <H3nry.g147@gmail.com>
2023-03-23 16:02:22 -0300

    system signature of connect

--- APIs/gamejolt_api.gd
+++ APIs/gamejolt_api.gd
@@ -2,10 +2,13 @@ extends Node
 
 var web = "https://api.gamejolt.com/api/game/v1_2/"
 var _http_request = HTTPRequest.new()
-var score = "score/add/"
+const FUNCTIONS = {
+	Data_store = ["data-store"]
+} 
 
 func connect_api(TYPE: String):
 	var LINK_BASE = web + TYPE
-  var LINK_INFO = LINK_BASE + 
-  var LINK_FINAL
-	pass
+	var LINK_INFO = LINK_BASE + "?game_id=" + _SaveCFG.gamejolt_info["game_id"]
+	var LINK_WITH_KEY = LINK_INFO + _SaveCFG.gamejolt_info["private_key"]
+	var LINK_FINAL = LINK_INFO + "&signature=" + LINK_WITH_KEY.sha1_text()
+	print(LINK_FINAL)
