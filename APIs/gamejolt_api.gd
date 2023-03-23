776598f7998f934e389e215e034a02d7664fdf54
Lucas Eduardo <54606139+Tio-Henry@users.noreply.github.com>
2023-03-23 07:19:00 -0300

    Actualizar gamejolt_api.gd

--- APIs/gamejolt_api.gd
+++ APIs/gamejolt_api.gd
@@ -2,8 +2,10 @@ extends Node
 
 var web = "https://api.gamejolt.com/api/game/v1_2/"
 var _http_request = HTTPRequest.new()
-
+var score = "score/add/"
 
 func connect_api(TYPE: String):
-	
+	var LINK_BASE = web + TYPE
+  var LINK_INFO = LINK_BASE + 
+  var LINK_FINAL
 	pass
