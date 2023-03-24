54586179b5e30586caa206017e045832088a116f
Lucas Eduardo <54606139+Tio-Henry@users.noreply.github.com>
2023-03-24 08:03:59 -0300

    update save_config.gd

--- scripts/save_config.gd
+++ scripts/save_config.gd
@@ -3,8 +3,8 @@ extends Node
 var file_cfg = ConfigFile.new()
 var gamejolt_file: String = "res://addons/.data_dev/Additional-APIs/gamejolt_info.cfg"
 var gamejolt_info = {
-	"game_id" : "ass",
-	"private_key" : "ass"
+	"game_id" : "",
+	"private_key" : ""
 }
 func _save_cfg(value):
 #	file_cfg.set_value()
