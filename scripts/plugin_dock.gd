5485a2b3dab9ad682a42915e7ba692be254996e9
Tio-Henry <H3nry.g147@gmail.com>
2023-03-24 14:12:36 -0300

    Correction System Save&Load

--- scripts/plugin_dock.gd
+++ scripts/plugin_dock.gd
@@ -1,14 +1,15 @@
 extends Control
 
 func _ready():
-  _SaveCFG.load_cfg()
+	_SaveCFG._load_cfg()
 
 func _on_save_btn_pressed():
-  _SaveCFG.gamejolt_info["game_id"] = 
-  _SaveCFG.gamejolt_info["private_key"] = 
+	_SaveCFG.gamejolt_info["game_id"] = $gamejolt_ui/PanelContainer/game_id/LineEdit.text
+	_SaveCFG.gamejolt_info["private_key"] = $gamejolt_ui/PanelContainer/private_key/LineEdit.text
 	_SaveCFG._save_cfg()
 	pass
 
 
 func _on_game_jolt_btn_toggled(button_pressed):
 	$gamejolt_ui.visible = button_pressed
+
--- scripts/save_config.gd
+++ scripts/save_config.gd
@@ -1,19 +1,27 @@
 extends Node
 
-var file_cfg = ConfigFile.new()
-var gamejolt_file: String = "res://addons/.data_dev/Additional-APIs/gamejolt_info.cfg"
+var _config = ConfigFile.new()
+var _file_cfg: String = "res://addons/.data_dev/Additional-APIs/_info.cfg"
 var gamejolt_info = {
 	"game_id" : "",
 	"private_key" : ""
 }
+var gamejolt_user = {
+	"username":"",
+	"user_token":""
+}
+
 func _save_cfg():
-	file_cfg.set_value("Game Jolt API","game_id", gamejolt_info["game_id"])
-  file_cfg.set_value("Game Jolt API","private_key",gamejolt_info["private_key"])
+	_config.set_value("Game Jolt API","game_id", gamejolt_info["game_id"])
+	_config.set_value("Game Jolt API","private_key",gamejolt_info["private_key"])
+	_config.save(_file_cfg)
 	pass
 	
-func _load_cfg(value, file):
-	var _file = FileAccess.open(file,FileAccess.READ)
-	value = _file.get_as_text()
+func _load_cfg():
+	_config.load(_file_cfg)
+	gamejolt_info["game_id"] = _config.get_value("Game Jolt API","game_id")
+	gamejolt_info["private_key"] = _config.get_value("Game Jolt API","private_key")
 
 func _login_GJ(username: String, user_token: String):
+	
 	pass
